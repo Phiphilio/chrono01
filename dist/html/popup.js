a5b1e91e0dda7c2e08bc0b0b673060c303a20028
erudit <philaoue@gmail.com>
2025-07-02 00:35:06 +0200

    modification du manifest.json pour permettre l'injection à l'ouverture de chaque popup

--- dist/html/popup.js
+++ dist/html/popup.js
@@ -6,3 +6,20 @@ chrome.runtime.sendMessage({ type: "demande-resultat" }, (response) => {
         document.getElementById("affichage").textContent = response.data;
     }
 });
+console.log("Popup ouvert !");
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const tab = tabs[0];
+    if (tab?.id) {
+        chrome.scripting
+            .executeScript({
+            target: { tabId: tab.id },
+            files: ["dist/script/script.js"],
+        })
+            .then(() => {
+            console.log("Script injecté !");
+        })
+            .catch((err) => {
+            console.error("Erreur d'injection :", err);
+        });
+    }
+});
--- src/html/popup.ts
+++ src/html/popup.ts
@@ -4,4 +4,23 @@
 chrome.runtime.sendMessage({ type: "demande-resultat" }, (response) => {
   if (response?.data) {
     document.getElementById("affichage")!.textContent = response.data;
+  }
+});
+
+console.log("Popup ouvert !");
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  const tab = tabs[0];
+  if (tab?.id) {
+    chrome.scripting
+      .executeScript({
+        target: { tabId: tab.id },
+        files: ["dist/script/script.js"],
+      })
+      .then(() => {
+        console.log("Script injecté !");
+      })
+      .catch((err) => {
+        console.error("Erreur d'injection :", err);
+      });
+  }
 });
